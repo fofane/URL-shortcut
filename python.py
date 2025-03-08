0dd97de3633dd8e4fb2b1efebf406334e02c8cce
i+pyladies <83703684+fofane@users.noreply.github.com>
2025-03-08 16:26:02 +0400

    Update python.py

--- python.py
+++ python.py
@@ -3,21 +3,21 @@ import subprocess
 import time
 
 # Path to Brave browser executable
-brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"  # Update with your Brave path
+path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"  # Update with your path
 
 # Define key-button mapping for each website
 key_url_map = {
-    'f2': 'https://classroom.btu.edu.ge/ge/student/me/course/scores/5/42',
-    'f3': 'https://classroom.btu.edu.ge/ge/student/me/course/groups/536/43',
-    'f4': 'https://classroom.btu.edu.ge/ge/student/me/course/index/8',
-    'f5': 'https://classroom.btu.edu.ge/ge/student/me/course/index/666',
-    'f6': 'https://classroom.btu.edu.ge/ge/student/me/course/index/67',
-    'f7': 'https://classroom.btu.edu.ge/ge/student/me/course/index/6'
+    'f1': 'https://www.example1.com',
+    'f2': 'https://www.example2.com',
+    'f3': 'https://www.example3.com',
+    'f4': 'https://www.example4.com',
+    'f5': 'https://www.example5.com',
+    'f6': 'https://www.example6.com'
 }
 
 # Function to open URL in Brave using subprocess
 def open_in_brave(url):
-    subprocess.run([brave_path, url])
+    subprocess.run([path, url])
 
 # Wait for a key press and open the corresponding URL in Brave
 while True:
